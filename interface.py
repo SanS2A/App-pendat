2b62596f30ef0224b1856e64cd7ac21f183ddf7c
20-032_San Sayidus Solatal A`la <84064493+SanS2A@users.noreply.github.com>
2022-11-20 20:52:09 +0700

    app
    add file

---
+++ interface.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import re
+import pandas as pd 
+import numpy as np
+
+st.title("""
+form input data, by OhMySannn
+""")
+
+#Fractional Knapsack Problem
+#Getting input from user
+Umur = int(st.number_input("Masukan Umur: ",0))
+Durasi = int(st.number_input("Durasi : ",0))
+Frekuensi = int(st.number_input("Frekuensi : ",0))
+Lokasi = int(st.number_input("Lokasi : ",0))
+Karakter = int(st.number_input("Karakter : ",0))
+Intensitas = int(st.number_input("Intensitas : ",0))
+Mual = int(st.number_input("Mual : ",0))
+Muntah = int(st.number_input("Muntah : ",0))
+Fanofobia = int(st.number_input("Fanofobia : ",0))
+Photophobia = int(st.number_input("Ketakutan dipotret : ",0))
+
+submit = st.button("submit")
+
+
+if submit:
+    st.info("Jadi,dinyakataan . ")
+
+
